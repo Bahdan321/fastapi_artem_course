4242bd84a2a5b6de52b40a1cd1e126441a153591
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-12 22:07:41 +0200

    adding migrations

--- my_practice/db/models/users.py
+++ my_practice/db/models/users.py
@@ -19,7 +19,7 @@ users = Table(
     Column("username", String, nullable=False),
     Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow()),
-    Column("tole_id", Integer, ForeignKey=("roles.id")),
+    Column("role_id", Integer, ForeignKey=("roles.id")),
 
 
 )
