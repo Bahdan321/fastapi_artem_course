de304669978baee112b534ebdd19f93a0940e69e
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-12 22:23:47 +0200

    final final refactoring users model

--- my_practice/db/models/users.py
+++ my_practice/db/models/users.py
@@ -20,7 +20,7 @@ users = Table(
     Column("username", String, nullable=False),
     Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey=("roles.id")),
+    Column("role_id", Integer, ForeignKey("roles.id")),
 
 
 )
