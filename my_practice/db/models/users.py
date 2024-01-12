6bb9b428d6a1b1ecf2e346bcdbbffea5a0388f60
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-01-12 22:46:40 +0200

    id refactoring users model

--- my_practice/db/models/users.py
+++ my_practice/db/models/users.py
@@ -6,30 +6,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 metadata = MetaData()
 
-users = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False),
-    Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("roles.id")),
-
-
-)
-
 class Roles(Base):
 
     metadata,
-    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     premissions: Mapped[JSON]
 
 class Users(Base):
 
     metadata,
-    id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
